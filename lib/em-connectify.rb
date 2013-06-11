53a80ce5b741c25f0b27ab0ac6e824dc9101713b
Andreas Kotes <andreas.kotes@nokia.com>
2013-06-11 11:03:37 +0200

    ruby1.8 compact

--- lib/em-connectify.rb
+++ lib/em-connectify.rb
@@ -1,4 +1,4 @@
 require 'eventmachine'
-require File.expand_path('em-connectify/connectify.rb', File.Dirname(__FILE__))
-require File.expand_path('em-connectify/connect.rb', File.Dirname(__FILE__))
-require File.expand_path('em-connectify/errors.rb', File.Dirname(__FILE__))
+require File.expand_path('em-connectify/connectify.rb', File.dirname(__FILE__))
+require File.expand_path('em-connectify/connect.rb', File.dirname(__FILE__))
+require File.expand_path('em-connectify/errors.rb', File.dirname(__FILE__))
